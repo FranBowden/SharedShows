3062e1af546cf4e7a4bbdac555aeecbca70c2b46
FranBowden <francesca.bowden22@bathspa.ac.uk>
2025-10-10 00:41:40 +0100

    deployment issues

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import filmsRouter from "./routes/Films";
 import cors from "cors";
-
+import { VercelRequest, VercelResponse } from "@vercel/node";
 const app = express();
 const PORT = 3000;
 
@@ -9,6 +9,12 @@ app.use(cors());
 
 app.use("/films", filmsRouter);
 
+/*
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+*/
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  app(req, res);
+};
